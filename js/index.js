0df7b67163babbd34219de89181345d1a1519a68
yangtao <1509440167@qq.com>
2019-05-22 21:32:22 +0800

    adf

--- js/index.js
+++ js/index.js
@@ -1,15 +1,15 @@
-﻿(function (doc, win) {
-    var docEl = doc.documentElement,
-        resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
-        recalc = function () {
-            var clientWidth = docEl.clientWidth;
-            if (!clientWidth) return;
-            docEl.style.fontSize = 100 * (clientWidth / 750) + 'px';
-        };
-    if (!doc.addEventListener) return;
-    win.addEventListener(resizeEvt, recalc, false);
-    doc.addEventListener('DOMContentLoaded', recalc, false);
-})(document, window);
+﻿// (function (doc, win) {
+//     var docEl = doc.documentElement,
+//         resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+//         recalc = function () {
+//             var clientWidth = docEl.clientWidth;
+//             if (!clientWidth) return;
+//             docEl.style.fontSize = 100 * (clientWidth / 750) + 'px';
+//         };
+//     if (!doc.addEventListener) return;
+//     win.addEventListener(resizeEvt, recalc, false);
+//     doc.addEventListener('DOMContentLoaded', recalc, false);
+// })(document, window);
 
 // ios点击事件不触发
 $(function() {  
