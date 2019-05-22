13881c0725f64283d560fa1bd39b7e3c7242f042
yangtao <1509440167@qq.com>
2019-05-22 14:24:29 +0800

    对对对

--- js/index.js
+++ js/index.js
@@ -1,16 +1,4 @@
-﻿// (function (doc, win) {
-//     var docEl = doc.documentElement,
-//         resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
-//         recalc = function () {
-//             var clientWidth = docEl.clientWidth;
-//             if (!clientWidth) return;
-//             docEl.style.fontSize = 100 * (clientWidth / 750) + 'px';
-//         };
-//     if (!doc.addEventListener) return;
-//     win.addEventListener(resizeEvt, recalc, false);
-//     doc.addEventListener('DOMContentLoaded', recalc, false);
-// })(document, window);
-(function (doc, win) {
+﻿(function (doc, win) {
     var docEl = doc.documentElement,
         resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
         recalc = function () {
@@ -18,7 +6,6 @@
             if (!clientWidth) return;
             docEl.style.fontSize = 100 * (clientWidth / 750) + 'px';
         };
-
     if (!doc.addEventListener) return;
     win.addEventListener(resizeEvt, recalc, false);
     doc.addEventListener('DOMContentLoaded', recalc, false);
